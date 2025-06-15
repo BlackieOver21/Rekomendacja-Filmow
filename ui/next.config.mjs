454efa2b10cadf187fde7faaa91f44d8136d438a
aSliwska <carmelwolf129@gmail.com>
2025-06-15 19:14:14 +0200

    correct server port

--- ui/next.config.mjs
+++ ui/next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
         return [
         {
             source: '/api/:path*',
-            destination: 'http://localhost:8080/api/:path*' // Proxy to Backend
+            destination: 'http://localhost:5000/api/:path*' // Proxy to Backend
         }
         ]
     },
